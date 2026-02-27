260c373990106c867e27ec42093fe1a77a3334e4
Alina <a72658695@gmail.com>
2026-02-27 09:37:01 +0300

    Step 6: Custom color scheme added

---
+++ app/src/main/java/com/startseva/lab10_11_and_kk/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.startseva.lab10_11_and_kk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.startseva.lab10_11_and_kk.data.Student
+import com.startseva.lab10_11_and_kk.data.student
+import com.startseva.lab10_11_and_kk.ui.theme.Lab10_11and_KKTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab10_11and_KKTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StudentIcon(
+    @DrawableRes studentIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small)),
+        painter = painterResource(studentIcon),
+        contentDescription = null
+    )
+}
+@Composable
+fun StudentInformation(
+    @StringRes studentName: Int,
+    studentAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text= stringResource(studentName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text=stringResource(R.string.years_old, studentAge),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+@Composable
+fun StudentItem (
+    student: Student,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier=modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
+    }
+
+}
+@Composable
+fun StudentApp(){
+    Scaffold { it ->
+        LazyColumn (contentPadding = it){
+            items(student) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+        }
+    }
+
+}
+@Preview
+@Composable
+fun StudentPreview(){
+    Lab10_11and_KKTheme(false) {
+        StudentApp()
+    }
+}
---
+++ app/src/main/java/com/startseva/lab10_11_and_kk/data/Student.kt
@@ -0,0 +1,12 @@
+package com.startseva.lab10_11_and_kk.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Student (
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val description: Int
+){
+}
